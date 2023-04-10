e88166199b4d670e58584405d5af0e848b7ee590
Moumouuu <robin.travail007@gmail.com>
2023-04-10 18:17:53 +0200

    add prisma adapter

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,10 +1,17 @@
 import NextAuth from "next-auth";
+
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import InstagramProvider from "next-auth/providers/instagram";
 
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
